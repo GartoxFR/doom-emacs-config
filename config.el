f911c22db005d85834e8607cc3ed0997fb07c2cc
Gartox <ewan71230@gmail.com>
2024-07-23 16:16:44 +0000

    Add theme and lsp

--- config.el
+++ config.el
@@ -32,7 +32,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq catppuccin-flavor 'mocha)
+(setq doom-theme 'catppuccin)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -76,9 +77,13 @@
 ;; they are implemented.
 
 (setq lsp-clients-clangd-args '("-j=3"
-				"--background-index"
-				"--clang-tidy"
-				"--completion-style=detailed"
-				"--header-insertion=never"
-				"--header-insertion-decorators=0"))
+                                "--background-index"
+                                "--clang-tidy"
+                                "--completion-style=detailed"
+                                "--header-insertion=never"
+                                "--header-insertion-decorators=0"))
 (after! lsp-clangd (set-lsp-priority! 'clangd 2))
+
+(add-hook! 'rust-mode-hook (tree-sitter-hl-mode))
+(add-hook! 'c-mode-hook (tree-sitter-hl-mode))
+(add-hook! 'c++-mode-hook (tree-sitter-hl-mode))
